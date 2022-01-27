e8dc28d52fb8e3c00af849c988871b6fa3963acc
Derek Mason <3derekmason@gmail.com>
2022-01-27 09:23:49 -0700

    add routes to each component on home page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import { createWebHistory, createRouter } from "vue-router";
 import Home from "@/views/Home.vue";
 import AboutPage from "@/views/AboutPage.vue";
 import CardFlip from "../components/CardFlip.vue";
+import HighwayScroll from "../components/HighwayScroll.vue";
+import WaterDrop from "../components/WaterDrop.vue";
+import WobbleIn from "../components/WobbleIn.vue";
 
 const routes = [
   {
@@ -18,6 +21,21 @@ const routes = [
     path: "/cardflip",
     name: "CardFlip",
     component: CardFlip,
+  },
+  {
+    path: "/highwayscroll",
+    name: "HighwayScroll",
+    component: HighwayScroll,
+  },
+  {
+    path: "/waterdrop",
+    name: "WaterDrop",
+    component: WaterDrop,
+  },
+  {
+    path: "/wobblein",
+    name: "WobbleIn",
+    component: WobbleIn,
   },
 ];
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,12 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <h2>HOME</h2>
+    <router-link to="/about">About</router-link>
     <div class="nav">
-      <router-link to="/about">About</router-link>
       <router-link to="/cardflip">Card Flip</router-link>
+      <router-link to="/highwayscroll">Highway Scroll</router-link>
+      <router-link to="/waterdrop">Water Drop</router-link>
+      <router-link to="/wobblein">Wobble In</router-link>
       <router-view />
     </div>
   </div>
@@ -16,9 +19,6 @@
 
 export default {
   name: "HomePage",
-  // components: {
-  //   HelloWorld,
-  // },
 };
 </script>
 
