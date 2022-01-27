5a9b645c3a8f1e66896f1e063f21f6d78ee4a536
Derek Mason <3derekmason@gmail.com>
2022-01-27 12:36:35 -0700

    update home page with prettier buttons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <h2>HOME</h2>
     <router-link to="/about">About</router-link>
     <div class="nav">
-      <router-link to="/cardflip">Card Flip</router-link>
-      <router-link to="/highwayscroll">Highway Scroll</router-link>
-      <router-link to="/waterdrop">Water Drop</router-link>
-      <router-link to="/wobblein">Wobble In</router-link>
+      <button class="homeBtn">
+        <router-link to="/cardflip">Card Flip</router-link>
+      </button>
+      <button class="homeBtn">
+        <router-link to="/highwayscroll">Highway Scroll</router-link>
+      </button>
+      <button class="homeBtn">
+        <router-link to="/waterdrop">Water Drop</router-link>
+      </button>
+      <button class="homeBtn">
+        <router-link to="/wobblein">Wobble In</router-link>
+      </button>
       <router-view />
     </div>
   </div>
@@ -23,6 +30,19 @@ export default {
 </script>
 
 <style>
+a {
+  text-decoration: none;
+}
+a:visited {
+  color: white;
+}
+a:active {
+  color: green;
+}
+a:hover {
+  color: #009688;
+}
+
 .home {
   display: flex;
   justify-content: center;
@@ -36,5 +56,24 @@ export default {
   justify-content: center;
   align-items: center;
   gap: 24px;
+}
+
+.homeBtn {
+  font-family: "Courier New", Courier, monospace;
+  font-weight: bold;
+  letter-spacing: 2px;
+  width: 104px;
+  height: 80px;
+  text-decoration: none;
+  border-radius: 8px;
+  border-color: #009688;
+  color: #fff;
+  box-shadow: 0 0 40px 40px #009688 inset, 0 0 0 0 #009688;
+  -webkit-transition: all 150ms ease-in-out;
+  transition: all 0.33s ease-in-out;
+}
+.homeBtn:hover {
+  box-shadow: 0 0 10px 0 #009688 inset, 0 0 10px 4px #009688;
+  color: #00796b;
 }
 </style>
